535eb4edb25acffc8a245c44580aa96502701927
Reachakhil <41184471+Reachakhil@users.noreply.github.com>
2020-07-30 21:49:48 +0530

    Updates

---
+++ precache-manifest.ef86b637f9a15def7f06a1b2b696f9f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b554e3b5dec67c12373ac6d8e47a08a",
+    "url": "/MyBio/index.html"
+  },
+  {
+    "revision": "45acf513170b21c5841a",
+    "url": "/MyBio/static/css/main.7652a449.chunk.css"
+  },
+  {
+    "revision": "ecdfc82e0b93e5d5fc68",
+    "url": "/MyBio/static/js/2.8dd3f609.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MyBio/static/js/2.8dd3f609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45acf513170b21c5841a",
+    "url": "/MyBio/static/js/main.fe1e1f8a.chunk.js"
+  },
+  {
+    "revision": "523544b512da2430b49f",
+    "url": "/MyBio/static/js/runtime-main.ee5f41c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyBio/static/media/logo.5d5d9eef.svg"
+  }
+]);
